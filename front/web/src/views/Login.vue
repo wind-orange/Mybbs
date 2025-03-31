d97bc1e6cd595bde069ecdee0fcfdb174395d197
zxn <2889062858@qq.com>
2025-03-31 21:07:25 +0800

    表单交互跳转

--- front/web/src/views/Login.vue
+++ front/web/src/views/Login.vue
@@ -13,6 +13,7 @@
         :rules="rules"
         ref="formDataRef"
       >
+        <!-- 登录 -->
         <!-- email -->
         <el-form-item prop="email">
           <el-input
@@ -26,39 +27,8 @@
             </template>
           </el-input>
         </el-form-item>
-        <!-- emailCode -->
-        <el-form-item prop="emailCode">
-          <div class="send-email-panel">
-            <el-input
-              size="large"
-              clearable
-              placeholder="请输入邮箱验证码"
-              v-model="formData.email"
-            >
-              <template #prefix>
-                <span class="iconfont icon-checkcode"></span>
-              </template>
-            </el-input>
-            <el-button class="send-email-btn" type="primary" size="large">
-              获取验证码
-            </el-button>
-          </div>
-        </el-form-item>
-        <!-- nickName -->
-        <el-form-item prop="nickName">
-          <el-input
-            size="large"
-            clearable
-            placeholder="请输入昵称"
-            v-model="formData.nickName"
-          >
-            <template #prefix>
-              <span class="iconfont icon-account"></span>
-            </template>
-          </el-input>
-        </el-form-item>
-        <!-- password -->
-        <el-form-item prop="password">
+        <!-- login password -->
+        <el-form-item prop="loginPassword" v-if="opType == 1">
           <el-input
             size="large"
             clearable
@@ -70,19 +40,66 @@
             </template>
           </el-input>
         </el-form-item>
-        <!-- rePassword -->
-        <el-form-item prop="rePassword">
-          <el-input
-            size="large"
-            clearable
-            placeholder="请再次输入密码"
-            v-model="formData.password"
-          >
-            <template #prefix>
-              <span class="iconfont icon-password"></span>
-            </template>
-          </el-input>
-        </el-form-item>
+        <!-- 注册、重置密码 -->
+        <div v-if="opType == 0 || opType == 2">
+          <!-- emailCode -->
+          <el-form-item prop="emailCode">
+            <div class="send-email-panel">
+              <el-input
+                size="large"
+                clearable
+                placeholder="请输入邮箱验证码"
+                v-model="formData.email"
+              >
+                <template #prefix>
+                  <span class="iconfont icon-checkcode"></span>
+                </template>
+              </el-input>
+              <el-button class="send-email-btn" type="primary" size="large">
+                获取验证码
+              </el-button>
+            </div>
+          </el-form-item>
+          <!-- nickName -->
+          <el-form-item prop="nickName" v-if="opType == 0">
+            <el-input
+              size="large"
+              clearable
+              placeholder="请输入昵称"
+              v-model="formData.nickName"
+            >
+              <template #prefix>
+                <span class="iconfont icon-account"></span>
+              </template>
+            </el-input>
+          </el-form-item>
+          <!-- register password -->
+          <el-form-item prop="registerPassword">
+            <el-input
+              size="large"
+              clearable
+              placeholder="请输入密码"
+              v-model="formData.registerPassword"
+            >
+              <template #prefix>
+                <span class="iconfont icon-password"></span>
+              </template>
+            </el-input>
+          </el-form-item>
+          <!-- register rePassword -->
+          <el-form-item prop="reRegiserPassword">
+            <el-input
+              size="large"
+              clearable
+              placeholder="请再次输入密码"
+              v-model="formData.reRegiserPassword"
+            >
+              <template #prefix>
+                <span class="iconfont icon-password"></span>
+              </template>
+            </el-input>
+          </el-form-item>
+        </div>
         <!-- checkCode -->
         <el-form-item prop="checkCode">
           <div class="check-code-panel">
@@ -104,17 +121,36 @@
           </div>
         </el-form-item>
         <!-- 记住我 -->
-        <el-form-item>
+        <el-form-item v-if="opType == 1">
           <div class="rememberMe">
             <el-checkbox v-model="formData.rememberMe">记住我</el-checkbox>
           </div>
+          <!-- 登录弹窗跳转 -->
           <div class="no-account">
-            <a href="javascript:void(0)" class="a-link">忘记密码</a>
-            <a href="javascript:void(0)" class="a-link">没有账号</a>
+            <a href="javascript:void(0)" class="a-link" @click="showPanel(2)">
+              忘记密码
+            </a>
+            <a href="javascript:void(0)" class="a-link" @click="showPanel(0)">
+              没有账号
+            </a>
           </div>
         </el-form-item>
+        <el-form-item v-if="opType == 0">
+          <a href="javascript:void(0)" class="a-link" @click="showPanel(1)">
+            已有帐号
+          </a>
+        </el-form-item>
+        <el-form-item v-if="opType == 2">
+          <a href="javascript:void(0)" class="a-link" @click="showPanel(1)">
+            去登陆
+          </a>
+        </el-form-item>
         <el-form-item>
-          <el-button class="op-btn" type="primary" size="large">登录</el-button>
+          <el-button class="op-btn" type="primary" size="large">
+            <span v-if="opType == 0">注册</span>
+            <span v-if="opType == 1">登录</span>
+            <span v-if="opType == 2">重置密码</span>
+          </el-button>
         </el-form-item>
       </el-form>
     </Dialog>
@@ -154,6 +190,13 @@ const opType = ref();
 const showPanel = (type) => {
   dialogConfig.show = true;
   opType.value = type;
+  if (type === 0) {
+    dialogConfig.title = "注册";
+  } else if (type === 1) {
+    dialogConfig.title = "登录";
+  } else {
+    dialogConfig.title = "重置密码";
+  }
 };
 defineExpose({ showPanel });
 </script>
